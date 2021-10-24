c308ad364f38a874eee6e5170e013708cd7a7899
viniciuszaki <viniciuszaki@gmail.com>
2021-10-24 13:44:28 -0300

    PontoFlutuanteCSharp

---
+++ TestaPontoFlutuante/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestaPontoFlutuante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario = 1250.70;
+
+            Console.WriteLine("meu salario é: " + salario);
+
+            //int valor = 12.5;             // não compila.
+
+            //int valor2 = 0.0;             // não compila.
+
+            //double teste = 125.50;        // compila
+
+            //int valor3 = teste;           // não compila.
+
+            double divisao = 5 / 2;
+            Console.WriteLine(divisao);
+
+            divisao = 5.0 / 2;
+            Console.WriteLine(divisao);
+
+            Console.ReadLine();
+        }
+    }
+}
